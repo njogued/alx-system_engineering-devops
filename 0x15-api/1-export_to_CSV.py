dd2dc2742ab66220d98c3e9eb633d1112e7b24ab
njogued <njogued@gmail.com>
2023-04-25 00:20:42 -0700

    Use str.format to fix module documentation error

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -14,17 +14,18 @@ if __name__ == "__main__":
     csv file - Comma Separated Values
     """
     usr = v[1]
-    usrs = get(f"https://jsonplaceholder.typicode.com/users/{usr}").json()
+    usrs = get("https://jsonplaceholder.typicode.com/users/{}".format(usr))
+    usrs = usrs.json()
     todos = get("https://jsonplaceholder.typicode.com/todos").json()
     username = usrs["username"]
     all_tasks = []
-    file_name = f"{usr}.csv"
+    file_name = "{}.csv".format(usr)
 
     for tasks in todos:
         if tasks["userId"] == int(usr):
             status = tasks["completed"]
             task = tasks["title"]
-            full = f'"{usr}","{username}","{status}","{task}"\n'
+            full = '"{}","{}","{}","{}"\n'.format(usr, username, status, task)
             all_tasks.append(full)
 
     with open(file_name, 'w') as f:
