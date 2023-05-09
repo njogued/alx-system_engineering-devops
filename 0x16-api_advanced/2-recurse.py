0f12837b63d4e6ca3479b0b4090aa3814770c964
njogued <njogued@gmail.com>
2023-05-09 14:50:47 -0700

    recurse

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Retrieve titles for posts
+a recursive function to return list containing the titles of all hot articles
+"""
+
+import requests as re
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """If not a valid subreddit, return None"""
+    custom_header = {"User-agent": "My agent"}
+    s = subreddit
+    a = after
+    u = "https://www.reddit.com/r/{}/hot.json?limit=50&after={}".format(s, a)
+    hotposts = re.get(u, headers=custom_header, allow_redirects=False)
+
+    if hotposts.status_code == 200:
+        post_info = hotposts.json()['data']
+        after = posts['after']
+        posts = post_info["children"]
+        for post in posts:
+            hot_list.append(post['data']['title'])
+        if after is not None:
+            recurse(s, hot_list, a)
+        return hot_list
+    else:
+        return None
