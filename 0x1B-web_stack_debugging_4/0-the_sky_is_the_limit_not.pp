62cef88d1cbc19fde8b9e9788da20ca054933260
njogued <njogued@gmail.com>
2023-05-18 07:06:46 -0700

    Puppet file

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# Puppet file to edit /etc/default/nginx
+exec { "editlimits":
+        command => 'sed -i "s/15/4096/g" /etc/default/nginx',
+        path => '/usr/local/bin/:/bin/'
+}
+exec { "restart_nginx":
+        command => 'nginx restart',
+        path => 'etc/init.d',
+        require => Exec['editlimits']
+}
